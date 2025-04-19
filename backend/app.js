5680648f9fd2410b4c27d928222a982499c6b8be
Chanveasna-ENG <137870404+Chanveasna-ENG@users.noreply.github.com>
2025-04-19 10:15:10 +0700

    update hyperlink to navigation

--- backend/app.js
+++ backend/app.js
@@ -16,16 +16,21 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Import routes
-const routes = require('./routes');
-app.use('/api', routes);
+// const routes = require('./routes');
+// app.use('/api', routes);
 
 // Webpage page routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
 });
-
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/contact.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/login.html'));
+});
+app.get('/reservation', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/reservation.html'));
 });
 
 app.get('/*splat', (req, res) => {
